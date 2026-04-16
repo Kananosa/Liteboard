243f64627b41aed04e5faa5665259dd217a4b041
kananosa <yangyuehua_1015@qq.com>
2026-04-16 22:42:10 +0800

    final commit all done yay

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Liteboard
-Liteboard is a keyboard with total 40 key,it is super lite....and functional
+Liteboard is a keyboard with total 40 key,it's super lite and functional (no RGB support)
+it absoulely can fit into ur pocket
 
-undone project.
+![3d-main](./picture/3d-main.png "liteboard-3d-picture")
 
+## Features:
+- 40 keys 12x4 layout (plank 40%)
+- Lite weight and **only 1.3cm thick**
+- An volume knob yay
+- haptic feedback support by the....wait iPhone haptic engine?
+## BOM:
+[liteboard-bom.csv]("./bom/liteboard-bom.csv")
+MIWU-MINI:an Chinese company shaft switch,i found it on Taobao.
+any Low shaft switch i think is okay.
+
+HDR-M_2.54_2x1:a pin header,connecting the haptic engine.
+any LRC haptic engine is okay for this keyboard.
+
+## Software:
+[source-code](./firmware/main.py)
+use kmk firmware,see https://github.com/KMKfw/kmk_firmware/ for more informaion.
--- firmware/main.py
+++ firmware/main.py
@@ -1,27 +1,48 @@
 print("Starting")
 
 import board
-
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
+from kmk.modules.layers import Layers
+from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
 
-keyboard.col_pins = (board.COL1,board.COL2,board.COL3,board.COL4,board.COL5,board.COL6,board.COL7,board.COL8,board.COL9,board.COL10,board.COL11,board.COL12)
-keyboard.row_pins = (board.ROW1,board.ROW2,board.ROW3,board.ROW4)
+keyboard.col_pins = (board.COL1, board.COL2, board.COL3, board.COL4, board.COL5, board.COL6,
+                     board.COL7, board.COL8, board.COL9, board.COL10, board.COL11, board.COL12)
+keyboard.row_pins = (board.ROW1, board.ROW2, board.ROW3, board.ROW4)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 layers = Layers()
 keyboard.modules.append(layers)
+
+encoder_handler = EncoderHandler()
+encoder_handler.pins = ((board.GP34, board.GP45, board.GP46),)
+encoder_handler.map = [((KC.VOLD, KC.VOLU, KC.MUTE),)]
+keyboard.modules.append(encoder_handler)
+
+______ = KC.TRNS
+
 keyboard.keymap = [
     [
-        KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,   KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,   KC.LBRC,    KC.RBRC,
-        KC.A,   KC.S,   KC.D,   KC.F,   KC.G,   KC.H,   KC.J,   KC.K,   KC.L,   KC.SCLN,    KC.QUOT,    KC.BSLS,
-        KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,   KC.N,   KC.M,   KC.COMM,    KC.DOT,     KC.SLSH 
-        KC.LCTL,     KC.LCMD,   KC.LALT,   KC.SPC,      KC.SPC,     KC.SPC,     KC.RALT     
-    ]
-
+        KC.TAB,  KC.TAB,  KC.Q,    KC.W,     KC.E,    KC.R,    KC.T,    KC.Y,    KC.U,    KC.I,    KC.O,    KC.P,
+        KC.CAPS, KC.CAPS, KC.A,    KC.S,     KC.D,    KC.F,    KC.G,    KC.H,    KC.J,    KC.K,    KC.L,    KC.ENT,
+        KC.LSFT, KC.LSFT, KC.Z,    KC.X,     KC.C,    KC.V,    KC.B,    KC.N,    KC.M,    KC.RSFT, KC.RSFT, KC.RSFT,
+        KC.LCTL, KC.LCMD, KC.LALT, KC.MO(1), KC.SPC,  KC.SPC,  KC.SPC,  KC.SPC,  KC.RALT, KC.BSPC, KC.BSPC, KC.BSPC,
+    ],
+    [
+        KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.MINS, KC.EQUAL,
+        KC.LBRC, KC.RBRC, KC.SCLN, KC.QUOT, KC.BSLS, KC.COMM, KC.DOT,  KC.SLSH, KC.UNDS, KC.PLUS, KC.NO,   KC.NO,
+        KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,   KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,  KC.F12,
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,
+    ],
+    [
+        KC.TILD, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.NO,
+        ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,
+        KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.NO,
+    ],
 ]
 
 if __name__ == '__main__':
